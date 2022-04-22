fe24216ab17f759a3e8c447f72b9a074a071b499
mxm-web-develop <mxm.web.develop@gmail.com>
2022-04-22 16:05:32 +0800

    312

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,63 @@
 // import { useI18n } from "vue-i18n";
 // import links from "@/data/links.json";
 import MxmCard from "@/components/src/Card/index.vue";
+import MxmShowbox from "@/components/src/Showbox/index.vue";
 
+import MxmNavbar from "@/components/src/Navbar/index.vue";
+const data = [
+  {
+    title: "永久的实名沟通渠道",
+    description:
+      "第三方提供实名认证，可验证聊天者真实身份，杜绝金融诈骗及商业骚扰。政府部门与居民之间建立可信数字化沟通渠道，精准发送通知，如接种疫苗、企业税收、法院传票等。",
+    img: "/img1.png",
+  },
+  {
+    title: "永久的实名沟通渠道",
+    description:
+      "第三方提供实名认证，可验证聊天者真实身份，杜绝金融诈骗及商业骚扰。政府部门与居民之间建立可信数字化沟通渠道，精准发送通知，如接种疫苗、企业税收、法院传票等。",
+    img: "/img1.png",
+  },
+  {
+    title: "永久的实名沟通渠道",
+    description:
+      "第三方提供实名认证，可验证聊天者真实身份，杜绝金融诈骗及商业骚扰。政府部门与居民之间建立可信数字化沟通渠道，精准发送通知，如接种疫苗、企业税收、法院传票等。",
+    img: "/img1.png",
+  },
+  {
+    title: "永久的实名沟通渠道",
+    description:
+      "第三方提供实名认证，可验证聊天者真实身份，杜绝金融诈骗及商业骚扰。政府部门与居民之间建立可信数字化沟通渠道，精准发送通知，如接种疫苗、企业税收、法院传票等。",
+    img: "/img1.png",
+  },
+  {
+    title: "永久的实名沟通渠道",
+    description:
+      "第三方提供实名认证，可验证聊天者真实身份，杜绝金融诈骗及商业骚扰。政府部门与居民之间建立可信数字化沟通渠道，精准发送通知，如接种疫苗、企业税收、法院传票等。",
+    img: "/img1.png",
+  },
+
+  {
+    title: "永久的实名沟通渠道",
+    description:
+      "第三方提供实名认证，可验证聊天者真实身份，杜绝金融诈骗及商业骚扰。政府部门与居民之间建立可信数字化沟通渠道，精准发送通知，如接种疫苗、企业税收、法院传票等。",
+    img: "/img1.png",
+  },
+  {
+    title: "永久的实名沟通渠道",
+    description:
+      "第三方提供实名认证，可验证聊天者真实身份，杜绝金融诈骗及商业骚扰。政府部门与居民之间建立可信数字化沟通渠道，精准发送通知，如接种疫苗、企业税收、法院传票等。",
+    img: "/img1.png",
+  },
+];
 </script>
 
 <template>
-
-  <div class="w-32 h-64 p-5">
-    <mxm-card mode="material" class="rounded-lg ">
-      <div>sdkfjksjk</div>
-      <div>dkfjk</div>
-    </mxm-card>
+  <div class="flex justify-center py-20">
+    <MxmShowbox :data="data"></MxmShowbox>
   </div>
+
+  <!-- <MxmCard>dfdf</MxmCard> -->
+  <!-- <Carousel></Carousel> -->
 </template>
 
 <style scoped>
@@ -22,6 +68,5 @@ import MxmCard from "@/components/src/Card/index.vue";
 * {
   margin: 0 !important;
   padding: 0;
-
 }
 </style>
